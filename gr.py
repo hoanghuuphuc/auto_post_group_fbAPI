18980f1f02520095b48ff4d3607a04bf2186f358
phuc <109650788+hoanghuuphuc@users.noreply.github.com>
2022-11-01 10:16:19 +0700

    Update gr.py

--- gr.py
+++ gr.py
@@ -8,8 +8,7 @@ import facebook as fb
 import gspread
 gc = gspread.service_account(filename='cre.json')
 sh = gc.open('ggls').sheet1
-token="EAAMJ1NPc1vUBAD3jZCVaa8s1yfLyfZCH6XKzlvCNZBddEruQ7x1XlaSZATpnmLJOME0d9ZBMDzKt1LIwH6BW0eEdGkZBpj2VkqKgxAa66qVcgM15ptdWoZAf4QTPZBDeqV6H1JBW5JStgOb0nRC1CFVaVHT7xXSpbvC2iYAgFxZCPzNM7MWAy6C4WAASirBOgXo43jEQVVX0E4OV9AIV5gXHLpuOskz8MwBVHjac69CpxF4NqILotySre"
-API=fb.GraphAPI(token)
+token=""
 # print(API.get_object("me"))
 # data=API.get_object("me",fields="groups")
 # for i in data["groups"]["data"]:
